40dc22a07da76f9d175c76b5d446c887f53fc7ce
katsura919 <katsuragik919@gmail.com>
2026-04-12 14:08:23 +0800

    add login

---
+++ lib/constants.ts
@@ -0,0 +1,12 @@
+export const AUTH_MOCK = {
+    user: {
+        id: "1",
+        name: "Jan Doe",
+        email: "jan@example.com",
+    },
+}
+
+export const ONBOARDING_METADATA = {
+    title: "The Peace-Driven Leader™",
+    description: "Your journey to activation begins here.",
+}
